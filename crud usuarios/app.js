0805b13a7adc91f5144f60f605bad29f04f7c901
MelissaGB <89152077+MelissaGB@users.noreply.github.com>
2021-10-14 23:36:46 -0500

    Back

--- crud usuarios/app.js
+++ crud usuarios/app.js
@@ -3,25 +3,28 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 
-var app = express()
+var cors = require('cors');
 
+
+var app = express()
+app.use(cors());
 //archivos de rutas
 
 var user_routes = require('./routes/user')
-
 
 //midlewares
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
+
 //CORDS
-app.use((req, res, next)=>{
+/* app.use((req, res, next)=>{
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-});
+}); */
 
 //rutas
 
